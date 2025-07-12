b4cda2fae4247f4f080730c630b414a3c86cbc31
xqrock <58764635+xqrock@users.noreply.github.com>
2025-07-12 14:21:07 +0800

    Create 2305part2.sh

---
+++ 2305part2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+##-----------------Del duplicate packages------------------
+rm -rf feeds/packages/net/open-app-filter
+##-----------------DIY-----------------
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/mosdns
+# rm -rf ./feeds/packages/net/shadowsocks-libev
+# rm -rf ./feeds/packages/net/shadowsocks-rust
+# rm -rf ./feeds/packages/net/shadowsocksr-libev
+# rm -rf ./feeds/luci/applications/luci-app-passwall
+# rm -rf ./feeds/luci/applications/luci-app-passwall2
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
