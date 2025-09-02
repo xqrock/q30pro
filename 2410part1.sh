ad296f440e751793a0b39bf017c1b75dabcf1030
xqrock <58764635+xqrock@users.noreply.github.com>
2025-09-02 14:13:10 +0800

    Update 2410part1.sh

--- 2410part1.sh
+++ 2410part1.sh
@@ -33,10 +33,10 @@ git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-adv
 # git clone https://github.com/linkease/istore.git package/istore
 # git clone https://github.com/linkease/istore-ui.git package/istore-ui
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-# git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-adguardhome
-# merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/adguardhome
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-adguardhome
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/adguardhome
